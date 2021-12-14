2b7860294fcde451d479a8cd1ee4ad28498f7624
Vincent Mansi <vincent.mansi@edu.devinci.fr>
2021-12-14 18:37:08 +0100

    exercice7

---
+++ exercise-7/my_ansible_advenced.sh
@@ -0,0 +1,14 @@
+if [ $# = 2 ]
+ then
+echo "Hello $2 $1, have a nice day !"
+ else if
+echo "Syntaxe : $0 firstname lastname"
+return 1
+else if($2 = $1)
+echo "Syntaxe : $0 fistname lastname"
+then 
+else if
+mkdir -p
+cat
+return 0
+fi 
