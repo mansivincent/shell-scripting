e2a3ddc628aa25178947269c48c5eae3a5dd0ee6
Vincent Mansi <vincent.mansi@edu.devinci.fr>
2021-12-14 19:08:17 +0100

    exercice6

---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,4 @@
+mkdir documents/{gamez,images,work/{code,plannings}
+touch documents/gamez/csgo.exe
+touch  documents/work/plannings/{april.xlsx,february.xlsx,january.xlsx,march.xlsx}
+touch meeting_notes.txt
